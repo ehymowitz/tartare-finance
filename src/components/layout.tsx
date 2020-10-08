32502db25becefad3403e625181bcb02209a678e
Elie Hymowitz <ehymowitz@gmail.com>
2020-10-08 16:20:04 -0400

    init

---
+++ src/components/layout.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import Helmet from "react-helmet"
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Helmet defer={false} >
+        <title>Elie Hymowitz</title>
+        <meta name="description" content= "Website Description" />
+        <meta property="og:title" content='Website Title' />
+        <meta property="og:description" content="Website Description" />
+        <meta property="og:image" content='/webImage.png' />
+        <meta property="og:url" content='Website Url'/>
+        <meta property="og:type" content='website'/>
+      </Helmet>
+      {children}
+    </div>
+  )
+}
