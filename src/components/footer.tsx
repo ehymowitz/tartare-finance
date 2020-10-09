0227d6e940debc83c578d1d72967dcbd04ed9951
Elie Hymowitz <ehymowitz@gmail.com>
2020-10-09 10:31:30 -0400

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <a>
+        <h4>
+          Livestock Contract
+        </h4>
+      </a>
+      <a>
+        <h4>
+          Discord
+        </h4>
+      </a>
+      <a>
+        <h4>
+          Twitter
+        </h4>
+      </a>
+      <a>
+        <h4>
+          Github
+        </h4>
+      </a>
+      <a>
+        <h4>
+          Medium
+        </h4>
+      </a>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Nav from '../components/nav'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <Layout>
       <Nav/>
+      <Footer/>
     </Layout>
   )
 }
